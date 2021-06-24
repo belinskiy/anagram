a72a22fa0ac2f7a2f8ed277df37a4c5168d0b7ce
belinskiy <belinskiy.kr@gmail.com>
2021-06-24 22:29:52 +0300

    Added class Anagram and fixed some bugs

---
+++ StrRev/Anagram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrRev
+{
+    class Anagram
+    {
+        public static string ReverseWord(string s)
+        {
+            if (s == null || s.Length == 0)
+                return s;
+            char[] str = s.ToCharArray();
+
+            int i = 0;
+            int j = 0;
+            while (j <= str.Length)
+            {
+                if (j == str.Length || str[j] == ' ')
+                {
+                    Reverse(str, i, j - 1);
+                    i = j + 1;
+                }
+                j++;
+            }
+            return new string(str);
+        }
+        public static void Reverse(char[] str, int l, int r)
+        {
+            while (l < r)
+            {
+                //Ignore special characters
+                if (!char.IsLetter(str[l]))
+                {
+                    l++;
+                }
+                else if (!char.IsLetter(str[r]))
+                {
+                    r--;
+                }
+                //Both str[l] and str[r] are not special
+                else
+                {
+                    char tmp = str[l];
+                    str[l] = str[r];
+                    str[r] = tmp;
+                    l++;
+                    r--;
+                }
+            }
+        }
+    }
+}
--- StrRev/Program.cs
+++ StrRev/Program.cs
@@ -4,43 +4,13 @@ namespace StrRev
 {
     class Program
     {
-        public static void ReverseWord(char[] str)
-        {
-            // Initialize left and right pointers
-            int r = str.Length - 1, l = 0;
 
-            // Traverse string from both ends until 'l' and 'r'
-            while (l < r)
-            {
-                //Ignore special characters
-                if (!char.IsLetter(str[l]))
-                    l++;
-                else if (!char.IsLetter(str[r]))
-                    r--;
-
-                //Both str[l] and str[r] are not special
-                else
-                {
-                    char tmp = str[l];
-                    str[l] = str[r];
-                    str[r] = tmp;
-                    l++;
-                    r--;
-                }
-            }
-        }
         static void Main(string[] args)
         {
+            /*            string input = Console.ReadLine();*/
             string input = "  a1bcd    efg!h";
-            char[] charArray = input.ToCharArray();
 
-            Console.WriteLine("Input string: " + input);
-
-            ReverseWord(charArray);
-
-            string output = new string(charArray);
-
-            Console.WriteLine("Output string: " + output);
+            Console.WriteLine(Anagram.ReverseWord(input));
         }
     }
 }
